d7e5e962b9ac65f8ba4a78c092afae81949694b8
Kevin Gann <kevin.gann@rev.com>
2018-08-03 11:07:24 -0500

    Change paths and methods.

--- src/main/java/callback/CallbackController.java
+++ src/main/java/callback/CallbackController.java
@@ -17,23 +17,29 @@ public class CallbackController {
 
   private static final Logger LOG = LoggerFactory.getLogger(CallbackController.class);
 
-  @RequestMapping(value = "/callback", method = POST)
+  @RequestMapping(
+      value = "/successful",
+      method = {GET, POST})
   @ResponseBody
-  public ResponseEntity<Object> postCallack(@RequestBody String job) {
+  public ResponseEntity<Object> respondSuccessful(@RequestBody String job) {
     LOG.info("Received HTTP 200 callback for job [" + job + "].");
     return ResponseEntity.status(HttpStatus.OK).body(null);
   }
 
-  @RequestMapping(value = "/callback-bad-request", method = POST)
+  @RequestMapping(
+      value = "/bad-request",
+      method = {GET, POST})
   @ResponseBody
-  public ResponseEntity<Object> postCallbackBadRequest(@RequestBody String job) {
+  public ResponseEntity<Object> respondBadRequest(@RequestBody String job) {
     LOG.info("Received 400 callback for job [" + job + "].");
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
   }
 
-  @RequestMapping(value = "/callback-unauthorized", method = POST)
+  @RequestMapping(
+      value = "/unauthorized",
+      method = {GET, POST})
   @ResponseBody
-  public ResponseEntity<Object> postCallbackUnauthorized(@RequestBody String job) {
+  public ResponseEntity<Object> respondUnauthorized(@RequestBody String job) {
     LOG.info("Received 401 callback for job [" + job + "].");
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
   }
@@ -42,13 +48,15 @@ public class CallbackController {
       value = "/internal-server-error",
       method = {GET, POST})
   @ResponseBody
-  public ResponseEntity<Object> getInternalServerError() {
+  public ResponseEntity<Object> respondInternalServerError() {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
   }
 
-  @RequestMapping(value = "/gone")
+  @RequestMapping(
+      value = "/gone",
+      method = {GET, POST})
   @ResponseBody
-  public ResponseEntity<Object> respondWithGone() {
+  public ResponseEntity<Object> responsdGone() {
     return ResponseEntity.status(HttpStatus.GONE).body(null);
   }
 }
