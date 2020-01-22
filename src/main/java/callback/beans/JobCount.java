25332c9f739e887f54863137c6317a419078102f
Kevin Gann <kevin.gann@rev.com>
2020-01-22 07:55:35 -0500

    Only include non-null fields.

--- src/main/java/callback/beans/JobCount.java
+++ src/main/java/callback/beans/JobCount.java
@@ -1,7 +1,9 @@
 package callback.beans;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class JobCount {
 
   @JsonProperty("overall-count")
