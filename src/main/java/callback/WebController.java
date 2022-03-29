4eafbd94f16a3889f7f62446bafd798db99cafb5
Kevin Gann <kevin.gann@rev.com>
2022-03-29 15:20:06 -0400

    Basic UI.

--- src/main/java/callback/WebController.java
+++ src/main/java/callback/WebController.java
@@ -1,46 +1,28 @@
 package callback;
 
-import callback.beans.RoleAndEnvironment;
+import callback.beans.CookiesForRoleAndEnvironment;
+import callback.repository.CookiesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class WebController {
 
-  @RequestMapping(value = "/getDateAndTime")
-  public ModelAndView getDateAndTime() {
-
-    var now = LocalDateTime.now();
-    var dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-    var date_time = dtf.format(now);
-
-    var params = new HashMap<String, Object>();
-    params.put("date_time", date_time);
-
-    return new ModelAndView("showMessage", params);
-  }
+  @Autowired CookiesRepository cookieRepository;
 
   @GetMapping("/")
   public String index() {
-    return "redirect:/form";
+    return "redirect:/cookies/list";
   }
 
-  @GetMapping("/form")
-  public String formGet() {
-    return "form";
-  }
-
-  @PostMapping("/form")
-  public String formPost(RoleAndEnvironment roleAndEnvironment, Model model) {
-    model.addAttribute("roleAndEnvironment", roleAndEnvironment);
-    return "form";
+  @GetMapping("/cookies/list")
+  public String cookiesList(Model model) {
+    List<CookiesForRoleAndEnvironment> cookies = cookieRepository.findAll();
+    model.addAttribute("cookies", cookies);
+    return "cookies-list";
   }
 }
